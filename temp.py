2bb33f4dd1ca8797340c7509318e4d8919326ddc
Alexander Dergachev <dergachev88@gmail.com>
2014-10-06 17:21:35 +0400

    python, simple plotting
    draw simple graph

--- temp.py
+++ temp.py
@@ -5,4 +5,12 @@ Spyder Editor
 This is a temporary script file.
 """
 
-print("Hello, Git!")
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.array([tmp for tmp in xrange(10)])
+y = np.array([tmp**2 for tmp in xrange(10)])
+
+plt.plot(x, y)
+plt.show()
